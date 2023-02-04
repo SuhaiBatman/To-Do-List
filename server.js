94f2a845617814d6ffadea04fba291e8e4c1871e
Arvin Saju <96351710+ASaju26@users.noreply.github.com>
2023-02-04 02:34:39 +0000

    Added code to server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
+
 const express = require("express");
+
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
 // initialize middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("MARVIN SUCKKS!!!"));
+app.get("/", (req, res) => res.send("Sushi is sus!!!"));
 
 // setting up port
 const PORT = process.env.PORT || 8000;
