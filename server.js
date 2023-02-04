1cf3eff775ae1348a2f3eb66a08e4c1524aed7d3
Arvin Saju <96351710+ASaju26@users.noreply.github.com>
2023-02-04 04:04:08 +0000

    Adding cors to allow for api calls from frontend

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 require('dotenv').config();
 
 const express = require("express");
+const cors = require("cors");
 
 const connectDB = require("./config/db");
 
 const app = express();
 
+//routers
 const todo = require("./routers/todo");
 
+//connect to database
 connectDB();
+
+//cors
+app.use(cors({ origin: true, credentials: true }));
+
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Sushi is sus!!!"));
+
+//use routers
+app.use("/api/todo", todo);
 
 // setting up port
 const PORT = process.env.PORT || 8000;
