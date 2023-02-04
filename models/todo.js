551576b1bab021c96306b7b7f37aebabed388090
Arvin Saju <96351710+ASaju26@users.noreply.github.com>
2023-02-04 07:11:26 +0000

    Fixed a small typo casing app to crash

--- models/todo.js
+++ models/todo.js
@@ -9,6 +9,6 @@ const TodoSchema = new mongoose.Schema({
         type: String,
     },
 });
-const Todo = mongoose.model("todo", TodoSchema);
+const Todo = mongoose.model("Todo", TodoSchema);
 
 module.exports = Todo;
